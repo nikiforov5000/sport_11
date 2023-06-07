6d72e80e1e59b71cc671ba523966c0fb309a0071
boris <nikiforov5000@gmail.com>
2023-06-07 13:51:03 +0300

    clean

--- lib/screens/web_view_screen.dart
+++ lib/screens/web_view_screen.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-// import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewScreen extends StatefulWidget {
   static const String id = '/web_view_screen';
@@ -15,61 +14,13 @@ class WebViewScreen extends StatefulWidget {
 }
 
 class _WebViewScreenState extends State<WebViewScreen> {
-  // final WebViewController _controller = WebViewController();
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    // initiateWebView();
-  }
-
-  // void initiateWebView() {
-  //   _controller
-  //     ..setJavaScriptMode(JavaScriptMode.unrestricted)
-  //     ..setBackgroundColor(Color(0x00112233))
-  //     ..setNavigationDelegate(
-  //       NavigationDelegate(
-  //         onProgress: (int progress) {
-  //           debugPrint('Webview is loading: $progress%');
-  //         },
-  //         onPageStarted: (String url) {
-  //           debugPrint('Page startet loading: $url');
-  //         },
-  //         onPageFinished: (String url) {
-  //           debugPrint('Page finished loading: $url');
-  //         },
-  //         onWebResourceError: (WebResourceError error) {
-  //           debugPrint('''
-  //           Page resorce error:
-  //             code: ${error.errorCode}
-  //             description: ${error.description}
-  //             errorType: ${error.errorType}
-  //             isForMainFrame: ${error.isForMainFrame}
-  //           ''');
-  //         },
-  //         onNavigationRequest: (NavigationRequest request) {
-  //           debugPrint('allowing navigation to ${request.url}');
-  //           return NavigationDecision.navigate;
-  //         },
-  //         onUrlChange: (UrlChange change) {
-  //           debugPrint('url change to ${change.url}');
-  //         },
-  //       )
-  //     )
-  //     ..loadRequest(Uri.parse(widget._url));
-  //
-  // }
-
   late InAppWebViewController _webViewController;
-  // late PullToRefreshController _pullToRefreshController;
   double progress = 0;
 
   Future<bool> onBackPressed() async {
     _webViewController.goBack();
     return false;
   }
-
 
   @override
   Widget build(BuildContext context) {
